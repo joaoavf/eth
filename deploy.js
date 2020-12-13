38d24fdd8e62d700e2f16ea2b51859019690e892
João Ferreira <joao.avf@gmail.com>
2020-12-13 16:19:13 -0300

    Converting project from Inbox to Lottery contract

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile')
+
+const provider = new HDWalletProvider(
+    'burden robot begin urban target federal frozen version master mix clump unhappy',
+    'https://goerli.infura.io/v3/4fa597b8c1a74f49bfc6a9e142e56b87'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0])
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({data: '0x' + bytecode}) // add 0x bytecode
+     .send({from: accounts[0]}); // remove 'gas'
+
+    console.log('Contract deployed to', result.options.address);
+}
+
+
+deploy();
+
+
--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -5,7 +5,7 @@ const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
 
 let accounts;
-let inbox;
+let lottery;
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -13,23 +13,16 @@ beforeEach(async () => {
 
   // Use one of those accounts to deploy
   // the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: ['Hi there!']
-    })
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
   });
 
 
-describe('Inbox', () => {
+describe('Lottery', () => {
     it('deploys a contract', () => {
-        assert.ok(inbox.options.address);
+        assert.ok(lottery.options.address);
     }
     );
 
-    it('has a default message', async () => {
-      const message = await inbox.methods.message().call();
-      assert.strictEqual(message, 'Hi there!');
-    });
   });
