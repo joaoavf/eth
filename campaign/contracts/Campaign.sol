76b02724d03a5154c191a4a357f9419a366dff9d
João Ferreira <joao.avf@gmail.com>
2020-12-15 12:56:46 -0300

    Finishing section 5. Campaign contract ready.

--- campaign/contracts/Campaign.sol
+++ campaign/contracts/Campaign.sol
@@ -1,25 +1,41 @@
 pragma solidity 0.4.17;
 
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);       
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
 contract Campaign {
     struct Request {
         string description;
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
     }
 
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
     mapping(address => bool) public approvers;
+    uint public approversCount;
 
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    function Campaign(uint minimum) public {
-        manager = msg.sender;
+    function Campaign(uint minimum, address manager) public {
+        manager = manager;
         minimumContribution = minimum;
 
     }
@@ -28,6 +44,7 @@ contract Campaign {
         require(msg.value > minimumContribution);
 
         approvers[msg.sender] = true;
+        approversCount++;
     }
 
     function createRequest(string description, uint value, address recipient) 
@@ -36,9 +53,30 @@ contract Campaign {
         Request memory request = Request({description: description,
                                    value: value,
                                    recipient: recipient:
-                                   complete: false})
+                                   complete: false,
+                                   approvalCount: 0})
 
         requests.push(request);
 
     }
-}
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restrict {
+        Request storage request = requests[index];
+
+        require(!request.complete);
+        require(request.approvalCount > (approversCount / 2));
+
+        request.recipient.transfer(request.value);
+        request.complete = true;  
+    }
+};
