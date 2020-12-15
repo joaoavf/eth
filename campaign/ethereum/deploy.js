421e3b60897d2a9bd2bc15162901cf9a590a4161
João Ferreira <joao.avf@gmail.com>
2020-12-15 14:40:45 -0300

    Finishing section 6. Deployed FactoryCampaign.

--- campaign/ethereum/deploy.js
+++ campaign/ethereum/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile')
+const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
     'burden robot begin urban target federal frozen version master mix clump unhappy',
@@ -13,13 +13,11 @@ const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
     console.log('Attempting to deploy from account', accounts[0])
 
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: '0x' + bytecode }) // add 0x bytecode
+    const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+        .deploy({ data: '0x' + compiledFactory.bytecode }) // add 0x bytecode
         .send({ from: accounts[0] }); // remove 'gas'
 
     console.log('Contract deployed to', result.options.address);
-    console.log(interface);
 }
-
 
 deploy();
